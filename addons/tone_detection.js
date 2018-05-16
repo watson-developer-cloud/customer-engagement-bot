a9373957030428da2d41cd543204ca0cdf9defae
Richie Verma <richie.v@ibm.com>
2018-05-16 15:44:39 -0700

    Removed eslint errors

--- addons/tone_detection.js
+++ addons/tone_detection.js
@@ -1,12 +1,12 @@
 /**
  * Copyright 2015 IBM Corp. All Rights Reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -20,26 +20,6 @@
 var Promise = require('bluebird');
 
 /**
- * Thresholds for identifying meaningful tones returned by the Watson Tone
- * Analyzer. Current values are based on the recommendations made by the Watson
- * Tone Analyzer at
- * https://www.ibm.com/watson/developercloud/doc/tone-analyzer/understanding-tone.shtml
- * These thresholds can be adjusted to client/domain requirements.
- */
-var PRIMARY_EMOTION_SCORE_THRESHOLD = 0.5;
-// var LANGUAGE_HIGH_SCORE_THRESHOLD = 0.75;
-// var LANGUAGE_NO_SCORE_THRESHOLD = 0.0;
-// var SOCIAL_HIGH_SCORE_THRESHOLD = 0.75;
-// var SOCIAL_LOW_SCORE_THRESHOLD = 0.25;
-
-/**
- * Labels for the tone categories returned by the Watson Tone Analyzer
- */
-var EMOTION_TONE_LABEL = 'emotion_tone';
-// var LANGUAGE_TONE_LABEL = 'language_tone';
-// var SOCIAL_TONE_LABEL = 'social_tone';
-
-/**
  * Public functions for this module
  */
 module.exports = {
@@ -51,7 +31,7 @@ module.exports = {
 /**
  * invokeToneAsync is an asynchronous function that calls the Tone Analyzer
  * service and returns a Promise
- * 
+ *
  * @param {Json}
  *                conversationPayload json object returned by the Watson
  *                Conversation Service
@@ -67,7 +47,7 @@ function invokeToneAsync(conversationPayload, toneAnalyzer) {
     toneAnalyzer.tone_chat({
       utterances: [
         { text: conversationPayload.input.text, user: 'customer'}
-    ]}, (error, data) => {
+      ]}, (error, data) => {
       if (error) {
         reject(error);
       } else {
@@ -78,27 +58,18 @@ function invokeToneAsync(conversationPayload, toneAnalyzer) {
 }
 
 /**
- * updateUserTone processes the Tone Analyzer payload to pull out the emotion,
- * language and social tones, and identify the meaningful tones (i.e., those
- * tones that meet the specified thresholds). The conversationPayload json
- * object is updated to include these tones.
- * 
+ * updateUserTone processes the Tone Analyzer payload to pull out the meaningful
+ * tones. The conversationPayload json object is updated to include these tones.
+ *
  * @param {Json}
  *                conversationPayload json object returned by the Watson
  *                Conversation Service
  * @param {Json}
  *                toneAnalyzerPayload json object returned by the Watson Tone
  *                Analyzer Service
- * @param {boolean}
- *                maintainHistory set history for each user turn in the history
- *                context variable
  * @returns {void}
  */
-function updateUserTone(conversationPayload, toneAnalyzerPayload, maintainHistory) {
-  var emotionTone = null;
-  //var languageTone = null;
-  //var socialTone = null;
-
+function updateUserTone(conversationPayload, toneAnalyzerPayload) {
   if (!conversationPayload.context) {
     conversationPayload.context = {};
   }
@@ -109,24 +80,24 @@ function updateUserTone(conversationPayload, toneAnalyzerPayload, maintainHistor
 
   // For convenience sake, define a variable for the user object
   var user = conversationPayload.context.user;
-  var detectedTones = toneAnalyzerPayload.utterances_tone[0].tones
+  var detectedTones = toneAnalyzerPayload.utterances_tone[0].tones;
   var userTone = null;
 
   if (detectedTones.length == 0) {
-    userTone = "none"
+    userTone = 'none';
   } else {
     userTone = detectedTones[0].tone_id;
-      }
-      
+  }
+
   user.tone.emotion.current = userTone;
   conversationPayload.context.user = user;
   return conversationPayload;
-  }
-   
+}
+
 /**
  * initToneContext initializes a user object containing tone data (from the
  * Watson Tone Analyzer)
- * 
+ *
  * @returns {Json} user json object with the emotion, language and social tones.
  *          The current tone identifies the tone for a specific conversation
  *          turn, and the history provides the conversation for all tones up to
@@ -143,6 +114,6 @@ function initUser() {
       //},
       //'social': {
       //  'current': null
-      }
     }
   };
+}
